2769cdd43208351ad0af6a7507e7ccce47a5edff
rkyzk <reikoyazaki@yahoo.com>
2022-10-27 09:46:23 +0000

    test if image shows up by clicking

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let card1 = document.getElementById("card1");
+card1.addEventListener("click", function() {
+  let html = `<img src="../images/cherries.png" alt="cherries">`;  
+  card1.innerHTML = html;
+});
+
+console.log("hello");
