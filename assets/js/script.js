855e952e20791c53a0c6b824163576267d04a047
rkyzk <reikoyazaki@yahoo.com>
2022-10-27 12:40:36 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 
 let card1 = document.getElementById("card1");
-
 card1.addEventListener("click", displayImg);
+
 function displayImg () {
-  let newElement = document.createElement("div");
-  let html = "<img src='../images/cherries.png' alt='cherries'>";  
-  newElement.innerHTML = html;
-  card1.appendChild(newElement);
+  if (card1.innerHTML !== undefined) {
+  let cherries = document.createElement("img");
+  cherries.src = "../images/cherries.png";
+  cherries.alt = "cherries";
+  card1.style.backgroundColor = "none";
+  card1.appendChild(cherries);
+  } else {
+    let images = document.getElementsByTagName("img")[0];
+    images.remove();
+    card1.style.backgroundColor = "rgb(38, 132, 180)";
+  }
 }
+
+
