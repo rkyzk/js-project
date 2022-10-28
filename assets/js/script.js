be74d08fc96f4d3938039f9dca00514067010281
rkyzk <reikoyazaki@yahoo.com>
2022-10-28 13:10:03 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,15 @@ let card1 = document.getElementById("card1");
 card1.addEventListener("click", displayImg);
 
 function displayImg () {
-  if (card1.innerHTML !== undefined) {
-  let cherries = document.createElement("img");
-  cherries.src = "../images/cherries.png";
-  cherries.alt = "cherries";
-  card1.style.backgroundColor = "none";
-  card1.appendChild(cherries);
+  if (card1.innerHTML === "") {
+    let cherries = document.createElement("img");
+    cherries.src = "assets/images/cherries.png";
+    cherries.alt = "cherries";
+    card1.style.backgroundColor = "beige";
+    card1.appendChild(cherries);
   } else {
-    let images = document.getElementsByTagName("img")[0];
-    images.remove();
+    card1.removeChild(element.childNodes[0]);
     card1.style.backgroundColor = "rgb(38, 132, 180)";
-  }
+  }  
 }
-
 
