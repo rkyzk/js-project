25f6632ea367d6ff2e97e04f9b2204394d62323f
rkyzk <reikoyazaki@yahoo.com>
2022-10-29 00:48:24 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ let card1 = document.getElementById("card1");
 card1.addEventListener("click", displayImg);
 
 function displayImg () {
-  if (card1.innerHTML === "") {
+if (card1.childNodes[0] === undefined) {
     let cherries = document.createElement("img");
     cherries.src = "assets/images/cherries.png";
     cherries.alt = "cherries";
     card1.style.backgroundColor = "beige";
     card1.appendChild(cherries);
   } else {
-    card1.removeChild(element.childNodes[0]);
+    card1.removeChild(card1.firstElementChild);
     card1.style.backgroundColor = "rgb(38, 132, 180)";
   }  
-}
+}  
 
