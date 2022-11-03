ccfb5fe2133e4b27889c5133843116e19fc11cc2
rkyzk <reikoyazaki@yahoo.com>
2022-11-03 10:47:06 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,132 @@
-let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let cardImages = [];
 
-function shuffle(num) {
-  for (let i = 0; i < num.length; i++) {
-    let j = Math.floor(Math.random() * num.length);
-    [num[i], num[j]] = [num[j], num[i]];
+document.addEventListener("DOMContentLoaded", function() {
+  cardImages = assignImgToCards();
+  let cards = document.getElementsByClassName("cards");   
+  for (let card of cards) {
+    card.addEventListener("mouseover", changeColor);
+    card.addEventListener("mouseout", changeColorBack); 
   }
-}
-
-shuffle(num);
-console.log(num);
-
-let colors = ["green", "green", "yellow", "yellow", "pink", "pink", "aquamarine",
-"aquamarine", "purple", "purple", "orange", "orange"];
-
-let cards = document.getElementsByClassName("cards");
-let twoColors = [];
-
-runGame();
+  let allCards = document.querySelector('#cards-wrapper'); 
+  allCards.addEventListener("click", flipCards);
+});
 
 /**
- * 
+ * assign images to the cards.
  */
-function runGame () {
-    for (i = 0; i < cards.length; i++) {
-      cards[i].addEventListener("click", function () {
-        let cardNum = parseInt(this.getAttribute('id').substr(4));
-        console.log(cardNum);
-        this.style.backgroundColor = colors[num[cardNum - 1]];
-        twoColors.push([cardNum, colors[num[cardNum - 1]]]);
-        console.log(twoColors);
-        if (twoColors.length === 2) {
-          if (twoColors[0][1] === twoColors[1][1]) {
-          console.log("yes!");
-          setTimeout (function () {
-            cards[twoColors[0][0] - 1].style.visibility = "hidden";
-            cards[twoColors[1][0] - 1].style.visibility = "hidden";
-          }, 1000);
-        } else {
-          console.log("keep trying");
-          setTimeout (function () {
-            console.log(cards[twoColors[0][0] - 1]);
-            console.log(cards[twoColors[1][0] - 1]);
-            cards[twoColors[0][0] - 1].style.backgroundColor = "beige";
-            cards[twoColors[1][0] - 1].style.backgroundColor = "beige";
-          }, 2000);
-        }     
-        setTimeout (function () {
-          twoColors = [];   
-          console.log(twoColors); 
-        }, 2200);
-      }
-    });
+function assignImgToCards() {
+  let images = ["elephant.png", "flamingo.png", "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png",
+    "elephant.png", "flamingo.png", "giraffe.png", "lion.png", "savanna-tree.jpg", "zebra.png"];
+  shuffle(images);
+  for (i = 0; i < images.length; i++) {
+    cardImages.push(images[i]);
+  }
+  return cardImages;
+}
+
+function shuffle(images) {
+  for (let i = 0; i < images.length; i++) {
+    let j = Math.floor(Math.random() * images.length);
+    [images[i], images[j]] = [images[j], images[i]];
   }
 }
 
+function changeColor(event) {
+  event.target.style.backgroundColor = "lightslategray";
+}
 
+function changeColorBack(event) {
+  event.target.style.backgroundColor = "lightsteelblue";
+}
+
+/**
+ * append an image to the flipped card, disable some eventListeners
+ * and call the check function after two cards have been flipped.
+ * @param {*} event 
+ */
+function flipCards(event){
+  // append an image to the target card 
+  if (event.target.getAttribute('src') === null) { 
+    let num = event.target.getAttribute('id').substr(4);
+    let image = document.createElement('img');
+    image.src = 'assets/images/' + cardImages[num - 1];
+    image.alt = cardImages[num - 1].substr(0, cardImages[num -1].length - 4);
+    image.style.backgroundColor = "beige";
+    event.target.appendChild(image);
+    event.target.classList.add('flipped');
+  }  
+  
+  // remove mouseover and mouseout events from the flipped card
+  event.target.removeEventListener('mouseover', changeColor); 
+  event.target.removeEventListener('mouseout', changeColorBack);  
+
+  // disable eventListener (flipCards) from all cards after two cards have been flipped
+  let flipped = document.getElementsByClassName('flipped');   
+  if (flipped.length === 2) {
+    let allCards = document.querySelector("#cards-wrapper");
+    allCards.removeEventListener("click", flipCards);
+    // check if the two cards are the same
+    check(flipped[0], flipped[1]);
+  }  
+}
+
+/**
+ * check if the two flipped cards are the same
+ * and handle them accordingly
+ * @param {} flipped0 
+ * @param {*} flipped1 
+ */
+function check(flipped0, flipped1) {
+  let cards = document.querySelector("#cards-wrapper");  
+  // if the two cards are the same, let them disappear after 1 second
+  if (flipped0.firstElementChild.getAttribute('src') === 
+    flipped1.firstElementChild.getAttribute('src')) {
+    setTimeout (function () {
+      flipped0.style.visibility = "hidden";
+      flipped1.style.visibility = "hidden";
+      flipped0.classList.remove('flipped');
+      flipped1.classList.remove('flipped');
+      // put back the eventListener (flipCards) to the rest of the cards
+      cards.addEventListener("click", flipCards);
+      // check if it's over and reward
+      reward();
+    }, 1000);
+  } else {
+    // if the cards are different, flip them back after 2 seconds.
+    setTimeout (function () {
+      flipped0.removeChild(flipped0.firstElementChild);
+      flipped1.removeChild(flipped1.firstElementChild);
+      flipped0.style.backgroundColor = "lightsteelblue";
+      flipped1.style.backgroundColor = "lightsteelblue";
+      flipped0.classList.remove('flipped');
+      flipped1.classList.remove('flipped');
+      // let eventListener (flipCards) resume
+      cards.addEventListener("click", flipCards);
+    }, 2000);
+  }
+}
+
+/**
+ * check if all the cards have disappeared, and if so,
+ * display a reward message.
+ */
+function reward() {    
+  let count = 0;
+  let cards = document.getElementsByClassName("cards");
+  for (let card of cards) {
+    if (card.style.visibility === "hidden") {
+      count++;
+    }
+  }
+  if (count === 12) {
+    let myNode = document.getElementById('cards-wrapper');
+    myNode.innerHTML = '';
+    let message = document.createElement('h2');
+    message.innerHTML = `<em>Well Done!</em>`;
+    let graphics = document.createElement('img');
+    graphics.src = 'assets/images/savanna-forest.jpg';
+    graphics.alt = 'savanna forest';
+    myNode.append(message);
+    myNode.append(graphics);
+  } 
+}
